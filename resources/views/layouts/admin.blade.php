c7262e393f1686ebc994b9e3ef90c5059fb82af7
FranciscoNoam <91475507+FranciscoNoam@users.noreply.github.com>
2022-01-19 13:46:44 +0300

    top_page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -37,10 +37,19 @@
 
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 
+    <link rel="stylesheet" href="{{asset('../assets/css/smooth_page.css')}}">
+
+    {{-- link fontawesome_all --}}
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
 
 </head>
 <body id="body-pd">
+    <button
+        type="button"
+        class="btn btn-floating btn-lg" id="btn-back-to-top">
+        <i class="far fa-arrow-up"></i>
+    </button>
     <div class="l-navbar" id="nav-bar">
         <div class="container-fluid ">
             <nav class="nav">
@@ -658,8 +667,33 @@
 
     <script src="{{asset('js/qcmStep.js')}}"></script>
     <link rel="stylesheet" href="{{asset('css/qcmStep.css')}}">
-</body>
 
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+<script>
+    let mybutton = document.getElementById("btn-back-to-top");
+    window.onscroll = function () {
+        scrollFunction();
+    };
+    function scrollFunction() {
+    if (
+        document.body.scrollTop > 300 ||
+        document.documentElement.scrollTop > 300
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+    mybutton.addEventListener("click", backToTop);
+    function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
 <script>
     document.addEventListener("DOMContentLoaded", function(event) {
 
