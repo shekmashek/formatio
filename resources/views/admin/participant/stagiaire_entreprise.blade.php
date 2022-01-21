701a2bae4f409d7d00178286a58cdb568c69427e
nic2000 <nicrah16@gmail.com>
2022-01-21 16:17:37 +0300

    add document ready function

--- resources/views/admin/participant/stagiaire_entreprise.blade.php
+++ resources/views/admin/participant/stagiaire_entreprise.blade.php
@@ -275,6 +275,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <script type="text/javascript">
+$(document).ready(function({
     $('#ajouter').on('click', function(e) {
         $('#resultat').empty();
         var cin = $('.cin').val();
@@ -315,6 +316,8 @@
         });
 
     });
+});
+
 
 </script>
 @endsection
