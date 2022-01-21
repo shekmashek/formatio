0617c41832cc2510aa5004781aed1218962e9705
nic2000 <nicrah16@gmail.com>
2022-01-21 16:21:00 +0300

    script

--- resources/views/admin/participant/stagiaire_entreprise.blade.php
+++ resources/views/admin/participant/stagiaire_entreprise.blade.php
@@ -274,7 +274,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<script type="text/javascript">
+<script>
 $(document).ready(function(){
     $('#ajouter').on('click', function(e) {
         $('#resultat').empty();
