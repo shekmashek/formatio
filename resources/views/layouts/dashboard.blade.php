02b397bffb4758fc328335f68715f2f9503d69da
FranciscoNoam <91475507+FranciscoNoam@users.noreply.github.com>
2022-01-24 09:48:22 +0300

    demo chart

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,64 +1,134 @@
 @extends('./layouts/admin')
 @section('content')
 
+{{-- 2 --}}
 <div class="row">
-	<div class="col-lg-3">
-		<div class="card-header"  style="background-color: rgb(125, 181, 253);">
-			<h1>Hello !</h1>
-		</div>
-		<div class="" style="background-color: rgb(70, 149, 252);">
-			<div class="card-body">
-				<canvas id="bar-chart" width="700" height="380"></canvas>
-			</div>
-		</div>
-	</div>
+    <div class="col-lg-4">
+        <div id="top_x_div"></div>
+    </div>
+    <div class="col-lg-4">
+        <div id="chart_div"></div>
+    </div>
+    <div class="col-lg-4">
+        <div id="columnchart_values"></div>
+    </div>
 </div>
-
-
-<style>
-    canvas {
-	display: block;
-	max-width: 600px;
-	/* margin: 40px auto; */
-}
-</style>
-
+{{-- fin 2 --}}
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script> --}}
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+ {{-- 1 --}}
+ <script type="text/javascript">
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawStuff);
+
+    function drawStuff() {
+      var data = new google.visualization.arrayToDataTable([
+        ['Opening Move', ''],
+        ["King's pawn (e4)", 44],
+        ["Queen's pawn (d4)", 31],
+        ["Knight to King 3 (Nf3)", 12],
+        ["Queen's bishop pawn (c4)", 10],
+        ['Other', 3]
+      ]);
+
+      var options = {
+        title: '',
+        width: 400,
+        legend: { position: 'none' },
+        chart: { title: '',
+                 subtitle: '' },
+        bars: 'horizontal', // Required for Material Bar Charts.
+        axes: {
+          x: {
+            0: { side: 'top', label: ''} // Top x-axis.
+          }
+        },
+        bar: { groupWidth: "90%" }
+      };
+
+      var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+      chart.draw(data, options);
+    };
+  </script>
+  {{-- fin 1 --}}
 
 
-<script>
-var bar_ctx = document.getElementById('bar-chart').getContext('2d');
 
-var purple_orange_gradient = bar_ctx.createLinearGradient(0, 0, 0, 600);
-purple_orange_gradient.addColorStop(0, '#801D68');
-purple_orange_gradient.addColorStop(1, 'white');
+{{-- 2 --}}
+<script type="text/javascript">
 
-var bar_chart = new Chart(bar_ctx, {
-    type: 'bar',
-    data: {
-        labels: ["J", "F", "M", "Avr"],
-        datasets: [{
-            label: '',
-            data: [1, 70, 3, 8],
-						backgroundColor: purple_orange_gradient,
-						hoverBackgroundColor: purple_orange_gradient,
-						hoverBorderWidth: 2,
-						hoverBorderColor: 'purple'
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+      ]);
+
+      // Set chart options
+      var options = {'title':'How Much Pizza I Ate Last Night',
+                     'width':400,
+                     'height':200};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
     }
-});
-</script>
+  </script>
+  {{-- fin 2 --}}
+
+{{-- 3 --}}
+<script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],
+        ["Copper", 8.94, "#b87333"],
+        ["Silver", 10.49, "silver"],
+        ["Gold", 19.30, "gold"],
+        ["Platinum", 21.45, "color: #e5e4e2"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Density of Precious Metals, in g/cm^3",
+        width: 400,
+        height: 200,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+  </script>
+{{-- fin 3 --}}
 @endsection
