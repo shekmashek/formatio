2cf045d4af56254dd1cbf2d4ef408714e9820090
nic2000 <nicrah16@gmail.com>
2022-01-25 09:53:22 +0300

    debug token google drive

---
+++ app/Mail/Contact.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Contact extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name,$objet,$message,$entreprise,$email)
+    {
+        $this->name = $name;
+        $this->objet = $objet;
+        $this->message = $message;
+        $this->entreprise = $entreprise;
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('contact@formation.mg')
+        ->subject('Message du site formation.mg')
+        ->view('emails.contact_email')
+        ->with([
+            'name' => $this->name,
+            'objet' => $this->objet,
+            'email' => $this->email,
+            'entreprise' => $this->entreprise,
+            'message' => $this->message
+
+        ]);
+    }
+}
