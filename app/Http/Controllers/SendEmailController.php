49299526200436fbafc726e8ca0a56a416930d19
eodielorina <eodielorinah08@gmail.com>
2022-01-25 09:28:06 +0300

    test

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -15,21 +14,7 @@ use Mail;
 
 class SendEmailController extends Controller
 {
-    // public function saveContact(Request $request) { 
-    //     Mail::send('contact_email',[
-            
-    //                     'name' => $request->name,
-    //                      'email' => $request->email,
-    //                       'objet' => $request->objet,
-    //                       'entreprise' => $request->entreprise,
-    //                      'msg' => $request->msg,
-    //     ],function($mail) use ($request){
-    //         $mail->from(env('MAIL_FROM_ADDRESS'),$request->name);
-    //         $mail->to('eodielorinah08@gmail.com')->subject('contact us mail');
-    //     });
-    //     return "message a ete bien envoyer";
-        
-    // }
+   
     public function sendMail(Request $request)
     {
 
