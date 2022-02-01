0955501f8f91005725c681bbb9be27d3caee16fc
FranciscoNoam <91475507+FranciscoNoam@users.noreply.github.com>
2022-02-01 11:58:17 +0300

    ok

--- resources/views/projet_session/index2.blade.php
+++ resources/views/projet_session/index2.blade.php
@@ -267,14 +267,14 @@
 </style>
 
 
-{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> --}}
-{{-- <meta name="csrf-token" content="{{ csrf_token() }}" /> --}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 
-<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+{{-- <script src="{{asset('assets/js/jquery.min.js')}}"></script>
 <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('assets/js/bootstrap.js')}}"></script>
 <script src="{{ asset('assets/js/jquery.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script> --}}
 
 <script type="text/javascript">
     $(".modifier").on('click', function(e) {
