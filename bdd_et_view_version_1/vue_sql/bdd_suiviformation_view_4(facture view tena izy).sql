26a34c23ecbe614e414c586b30b2d6642e5ca306
FranciscoNoam <91475507+FranciscoNoam@users.noreply.github.com>
2022-02-02 12:02:12 +0300

    edit view facture

--- bdd_et_view_version_1/vue_sql/bdd_suiviformation_view_4(facture view tena izy).sql
+++ bdd_et_view_version_1/vue_sql/bdd_suiviformation_view_4(facture view tena izy).sql
@@ -59,22 +59,6 @@ FROM
 RIGHT JOIN v_montant_pedagogique_facture mpf ON
     mpf.num_facture = mfa.num_facture  AND mpf.cfp_id = mfa.cfp_id;
 
--- CREATE OR REPLACE VIEW v_montant_brut_facture AS SELECT
---     mpf.cfp_id,
---     mpf.projet_id,
---     mpf.num_facture,
---     (
---         mpf.hors_taxe + IFNULL(mfa.hors_taxe, 0)
---     ) AS montant_brut_ht,
---     mpf.due_date,
---     mpf.invoice_date
--- FROM
---     v_montant_frais_annexe mfa
--- RIGHT JOIN v_montant_pedagogique_facture mpf ON
---     mpf.num_facture = mfa.num_facture AND mpf.projet_id = mfa.projet_id AND mpf.cfp_id = mfa.cfp_id;
-
-
-
 CREATE OR REPLACE VIEW v_remise_facture AS SELECT
 	cfp_id,
     projet_id,
