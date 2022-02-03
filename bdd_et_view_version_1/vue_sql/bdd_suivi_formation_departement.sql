bc71278f5cc8f53fdf45abd75b841bbde01c3856
nic2000 <nicrah16@gmail.com>
2022-02-03 16:58:16 +0300

    modif departement

--- bdd_et_view_version_1/vue_sql/bdd_suivi_formation_departement.sql
+++ bdd_et_view_version_1/vue_sql/bdd_suivi_formation_departement.sql
@@ -11,9 +11,3 @@ WHERE
     dep.entreprise_id = etp.id AND
     serv.departement_entreprise_id = dep.id;
 
-select * GROUP_CONCAT('nom_service') as nom_service from v_departement_service_entreprise 
-
-
-SELECT `nom_departement`, GROUP_CONCAT(`nom_service`) AS service
-FROM `v_departement_service_entreprise` 
-GROUP BY `nom_departement`
