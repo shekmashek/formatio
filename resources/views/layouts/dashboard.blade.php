a5528ed36cb2724e58060f89c964d46cfcc7b861
FranciscoNoam <91475507+FranciscoNoam@users.noreply.github.com>
2022-02-04 12:06:03 +0300

    cfp système tableau de bord

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,68 +1,161 @@
 @extends('./layouts/admin')
 @section('content')
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <style>
+        .nav-tabs .nav-link{
+            background-image:linear-gradient(60deg, #AA076B 40%, #61045F)
+            /* color: white; */
+        }
+        .navigation{
+            background-color: #f8f9fa;
+            /* background-image:linear-gradient(100deg, #AA076B, #61045F) */
+        }
+
+        .navigation:hover{
+            background-color: #f8f9fa;
+            /* background-color: #801D68; */
+        }
+
+        .system_{
+            text-align: left;
+            border: none;
+            border-bottom: 1px solid #801D68;
+        }
+        .system_num{
+            text-align: right;
+            color: #801d68;
+            font-size: 20px;
+            border-radius: 10px;
+            float: right;
+            position: relative;
+            bottom: .5rem;
+        }
+
+        .system_numero{
+            text-align: right;
+            color: white;
+            background-color: #9d207d;
+            border: none;
+            border-radius: 5px;
+            float: right;
+            padding-left: 5px;
+            padding-right: 5px
+        }
+    </style>
 
     <div class="p-0 m-4">
-        <ul class="nav nav-tabs d-flex flex-row navigation justify-content-end" id="myTab" style="font-size: 13px;">
+        <ul class="nav nav-tabs d-flex flex-row navigation justify-content-end " id="myTab" style="font-size: 13px;">
             <li class="nav-item">
-                <a href="#TBFinancière" class="nav-link active" data-bs-toggle="tab">Tableau de bord financière</a>
+                <a href="#TBFinancière" class="nav-link active" data-bs-toggle="tab" style="color: white"> <i class="fad fa-sliders-v"></i>&nbsp; TDB système</a>
             </li>
             <li class="nav-item">
-                <a href="#TBStatistique" class="nav-link " data-bs-toggle="tab">Tableau de bord statistique</a>
+                <a href="#TBStatistique" class="nav-link " data-bs-toggle="tab" style="color: white"> <i class="far fa-money-check"></i>&nbsp; TDB financier</a>
             </li>
             <li class="nav-item">
-                <a href="#TBQualité" class="nav-link " data-bs-toggle="tab">Tableau de bord Qualité</a>
+                <a href="#TBQualité" class="nav-link " data-bs-toggle="tab" style="color: white"> <i class="fad fa-book-alt"></i>&nbsp; TDB Qualité</a>
             </li>
         </ul><br>
 
         <div class="tab-content ">
             <div class="tab-pane fade show active" id="TBFinancière">
-                <div class="container-fluid">
+                <div class="container-fluid" style="font-size: 13px;">
                     <div class="row mt-2">
                         <div class="col-lg-4">
-                            <div class="form-control">
-                                <p class="text-center"><b style="font-size: 17px;">Tableau de bord financier</b></p>
-                                <p class="p-0 m-0 " style="font-size: 14px; font-weight: bold;">C.A actuel:
-                                    @php
-                                        foreach ($CA_actuel as $total) {
-                                            $total = $total->total_ttc;
-                                            echo $total . ' ';
-                                        }
-                                    @endphp Ar TTC</p>
-
-                                <p class="p-1 m-0" style="font-size: 13px;">C.A précedent:
-                                    @php
-                                        foreach ($CA_precedent as $totals) {
-                                            $totals = $totals->total_ttc;
-                                            echo $totals . ' ';
-                                        }
-                                    @endphp Ar TTC</p>
-                                <hr>
-                                <div id="chart_div"></div>
+                            <div class="shadow-sm p-3 mb-5 bg-body rounded" style="color: #801D68"> <b><i class="fad fa-users "></i> &nbsp; Collaborateur</b>
+                                <p class="p-0 m-3 system_ pb-1">Formateurs<span class="system_num"><i style="color: red;" class='bx bxs-user-x'></i></span></p>
+                                {{-- <p class="m-3 system_">Entreprise<span class="system_num"><i class='bx bxs-check-shield'></i></span></p>
+                                <p class="m-3 system_">Réferents<span class="system_num"><i class='bx bxs-user-check' ></i></span></p> ireto ny icone--}}
+                                <p class="m-3 system_ pb-1">Entreprise<span class="system_numero">70</span></p>
+                                <p class="m-3 system_ pb-1">Réferents<span class="system_numero">55</span></p>
                             </div>
                         </div>
                         <div class="col-lg-4">
-                            <div class="form-control">
-                                <p class="text-center"><b style="font-size: 17px;">Chiffre d'affaires par module</b></p>
-                                <p class="p-0 m-0 " style="font-size: 15px; font-weight: bold;">Top 10 modules</p>
-                                <hr>
-                                <div id="barchart_material"></div>
+                            <div class="shadow-sm p-3 mb-5 bg-body rounded" style="color: #801D68"><b> <i class="far fa-book-spells"></i> &nbsp; Catalogue </b>
+                                <p class="p-0 m-3 system_ pb-1">Publié<span class="system_numero">5</span></p>
+                                <p class="m-3 system_">EN cours<span class="system_num"><i style="color: rgb(2, 162, 21)" class='bx bxs-check-shield'></i></span></p>
+                                <p class="m-3 system_ pb-1">A venir<span class="system_numero">70</span></p>
                             </div>
                         </div>
                         <div class="col-lg-4">
-                            <div class="form-control">
-                                <p class="text-center"><b style="font-size: 17px;">Chiffre d'affaires par client</b></p>
-                                <p class="p-0 m-0 " style="font-size: 15px; font-weight: bold;">Top 10 clients</p>
-                                <hr>
-                                <div id="barchart_material_2"></div>
+                            <div class="shadow-sm p-3 mb-5 bg-body rounded" style="color: #801D68"><b> <i class="fad fa-address-card"></i>&nbsp; Facture </b>
+                                <p class="p-0 m-3 system_ pb-1">Payé<span class="system_numero">3</span></p>
+                                <p class="m-3 system_ pb-1">Non échu<span class="system_numero">7</span></p>
+                                <p class="m-3 system_ pb-1">Echu non payé<span class="system_numero">0</span></p>
                             </div>
                         </div>
                     </div>
-
+                </div>
+                {{-- 2 --}}
+                <div class="container-fluid" style="font-size: 13px;">
+                    <div class="row mt-2">
+                        <div class="col-lg-4">
+                            <div class="shadow-sm p-3 mb-5 bg-body rounded" style="color: #801D68"><b> <i class="fas fa-tasks"></i> &nbsp; Session Inter entreprise </b>
+                                <p class="p-0 m-3 system_ pb-1">Comlété<span class="system_numero">3</span></p>
+                                <p class="m-3 system_ pb-1">En cours<span class="system_numero">7</span></p>
+                                <p class="m-3 system_ pb-1">A venir<span class="system_numero">0</span></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="shadow-sm p-3 mb-5 bg-body rounded" style="color: #801D68"><b> <i class="fas fa-tasks"></i>&nbsp; Session Intra enreprise </b>
+                                <p class="p-0 m-3 system_ pb-1">Comlété<span class="system_numero">3</span></p>
+                                <p class="m-3 system_ pb-1">En cours<span class="system_numero">0</span></p>
+                                <p class="m-3 system_ pb-1">A venir<span class="system_numero">10</span></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-
         <div class="tab-pane fade" id="TBStatistique">
+            <div class="container-fluid">
+                <div class="row mt-2">
+                    <div class="col-lg-4">
+                        <div class="form-control">
+                            <p class="text-center"><b style="font-size: 17px;">TDB financier</b></p>
+                            <p class="p-0 m-0 " style="font-size: 14px; font-weight: bold;">C.A actuel:
+                                @php
+                                    foreach ($CA_actuel as $total) {
+                                        $total = $total->total_ttc;
+                                        echo $total . ' ';
+                                    }
+                                @endphp Ar TTC</p>
+
+                            <p class="p-1 m-0" style="font-size: 13px;">C.A précedent:
+                                @php
+                                    foreach ($CA_precedent as $totals) {
+                                        $totals = $totals->total_ttc;
+                                        echo $totals . ' ';
+                                    }
+                                @endphp Ar TTC</p>
+                            <hr>
+                            <div id="chart_div"  style="width: 200px; height: 200px;"></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="form-control">
+                            <p class="text-center"><b style="font-size: 17px;">CA par module</b></p>
+                            <p class="p-0 m-0 " style="font-size: 15px; font-weight: bold;">Top 10 modules</p>
+                            <hr>
+                            <div id="barchart_material"  style="width: 200px; height: 200px;"></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="form-control">
+                            <p class="text-center"><b style="font-size: 17px;">CA par client</b></p>
+                            <p class="p-0 m-0 " style="font-size: 15px; font-weight: bold;">Top 10 clients</p>
+                            <hr>
+                            <div id="barchart_material_2"  style="width: 200px; height: 200px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            {{-- fin --}}
+
+        </div>
+
+
+        <div class="tab-pane fade" id="TBQualité">
             <div class="container-fluid">
                 <div class="row mt-2">
                     <div class="col-lg-4">
@@ -108,26 +201,10 @@
 
             </div>
         </div>
-
-
-        <div class="tab-pane fade" id="TBQualité">
-
-        </div>
       </div>
     </div>
 
-<style>
-  .nav-tabs .nav-link{
-    color: white;
-  }
-  .navigation{
-    background-image:linear-gradient(100deg, #AA076B, #61045F)
-  }
 
-  .navigation:hover{
-    background-color: #801D68;
-  }
-</style>
     {{-- <div class="row mt-2">
     <div class="col-lg-4">
         <div class="form-control">
@@ -264,7 +341,7 @@
             };
 
             var classicOptions = {
-                width: 350,
+                width: 250,
                 series: {
                     0: {
                         targetAxisIndex: 0
@@ -375,7 +452,7 @@
         ]);
 
         var options = {
-            width: 350,
+            width: 250,
           chart: {
             title: '',
             subtitle: '',
