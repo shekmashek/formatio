e66b3406152bfbd1727773fb13c573f9462059a5
nic2000 <nicrah16@gmail.com>
2022-03-14 15:37:07 +0300

    legende bullet

--- resources/views/referent/dashboard_referent/dashboard_referent_budget_prev.blade.php
+++ resources/views/referent/dashboard_referent/dashboard_referent_budget_prev.blade.php
@@ -2,6 +2,14 @@
 @section('content')
 <script src='https://cdn.plot.ly/plotly-2.9.0.min.js'></script>
 <link rel="stylesheet" href="{{asset('css/style_dashboard.css')}}">
+<style>
+    .legende{
+        border: solid 10px;
+        border-color: #E1DDDD;
+        background-color: #E1DDDD;
+        width: 40px;
+    }
+</style>
 {{-- <link rel="stylesheet" href="ttps://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"> --}}
 
 
@@ -13,14 +21,21 @@
         <a href="{{ route('budget_previsionnel')}}" type="button" class="btn " style="font-size: 12px;"> <i class="fad fa-chart-bar" style="font-size: 10px;"></i>&nbsp;TDB budget previsionnel</a>    </div>
     @endcan
 </div>
-
-<div class="p-1 m-0">
-    <div class="container-fluid">
-        <div class="row">
-            <div id="chart_div"></div>
+<div class="container-fluid">
+    <div class="row mb-3">
+        <div class="col-md-12 d-flex flex-row">
+          <div class="legende me-3"></div>
+          <h5>Réalisé</h5>
+          <div class="legende me-3"></div>
+          <h5>Engagé</h5>
         </div>
     </div>
+    <div class="row">
+        <div id="chart_div"></div>
+    </div>
 </div>
+
+
 	<!-- Load plotly.js into the DOM -->
 
 <script>
@@ -31,7 +46,7 @@
         // value:  @php echo $total_budget[0]->total @endphp,
         domain: { x: [0, 1], y: [0, 1] },
         title: { text: "<b>Budget</b>" },
-        delta: { reference: 200 },
+        delta: { reference: @php echo $total_budget[0]->total @endphp },
         gauge: {
           shape: "bullet",
           axis: { range: [null, @php echo $total_budget[0]->total @endphp] },
@@ -43,14 +58,14 @@
           bar: { color: "grey" },
            steps: [
             { range: [0,1000000], color: "#E1DDDD" },
-            { range: [1000000,1200000], color: "#94928F" },
-            { range: [1200000,1700000], color: "#757472" }
+            { range: [1000000,1700000], color: "#94928F" }
+            // { range: [1200000,1700000], color: "#757472" }
           ]
         }
       }
     ];
 
-    var layout = { width: 600, height: 350 };
+    var layout = { width: 600, height: 250 };
     var config = { responsive: true };
     var myDiv = document.getElementById('chart_div');
     Plotly.newPlot(myDiv, data, layout, config);
