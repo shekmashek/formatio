a0b56a161434cc4306a323277610a95cf566df32
nic2000 <nicrah16@gmail.com>
2022-03-14 12:32:33 +0300

    bullet chart add threshold configuration

--- resources/views/referent/dashboard_referent/dashboard_referent_budget_prev.blade.php
+++ resources/views/referent/dashboard_referent/dashboard_referent_budget_prev.blade.php
@@ -1,5 +1,6 @@
 @extends('./layouts/admin')
 @section('content')
+<script src='https://cdn.plot.ly/plotly-2.9.0.min.js'></script>
 <link rel="stylesheet" href="{{asset('css/style_dashboard.css')}}">
 {{-- <link rel="stylesheet" href="ttps://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"> --}}
 
@@ -21,13 +22,13 @@
     </div>
 </div>
 	<!-- Load plotly.js into the DOM -->
-	<script src='https://cdn.plot.ly/plotly-2.9.0.min.js'></script>
+
 <script>
         var data = [
       {
         type: "indicator",
         mode: "number+gauge+delta",
-        value: @php echo $total_realise[0]->realise @endphp,
+        // value:  @php echo $total_budget[0]->total @endphp,
         domain: { x: [0, 1], y: [0, 1] },
         title: { text: "<b>Budget</b>" },
         delta: { reference: 200 },
@@ -35,20 +36,21 @@
           shape: "bullet",
           axis: { range: [null, @php echo $total_budget[0]->total @endphp] },
           threshold: {
-            line: { color: "red", width: 2 },
-            thickness: 0.75,
+            line: { color: "red", width: 5, height:200},
+            thickness: 1,
             value: @php echo $total_budget[0]->total @endphp
           },
-          // steps: [
-          //   { range: [0,@php echo $total_engage[0]->engage @endphp], color: "grey" },
-          //   { range: [50000,@php echo $total_realise[0]->realise @endphp], color: "grey" },
-          //   { range: [@php echo $total_realise[0]->realise @endphp,  @php echo $total_budget[0]->total @endphp], color: "blue" }
-          // ]
+          bar: { color: "grey" },
+           steps: [
+            { range: [0,1000000], color: "#E1DDDD" },
+            { range: [1000000,1200000], color: "#94928F" },
+            { range: [1200000,1700000], color: "#757472" }
+          ]
         }
       }
     ];
 
-    var layout = { width: 600, height: 250 };
+    var layout = { width: 600, height: 350 };
     var config = { responsive: true };
     var myDiv = document.getElementById('chart_div');
     Plotly.newPlot(myDiv, data, layout, config);
