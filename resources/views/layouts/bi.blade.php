8cb92d079c92a32a90d815fbdd334938a9fe7f8e
nic2000 <nicrah16@gmail.com>
2022-04-06 14:28:53 +0300

    modification page bi

--- resources/views/layouts/bi.blade.php
+++ resources/views/layouts/bi.blade.php
@@ -1,9 +1,14 @@
 @extends('./layouts/admin')
 @section('content')
+<style>
+    .bi-content{
+        margin: 100px 100px 100px 400px;
+    }
+</style>
 <div class="container-fluid pb-5">
     @can('isReferent')
         @if($iframe_etp->iframe == null)
-            <img src="{{asset('images/construction.png')}}" alt="">
+           <div class="bi-content"><h1>Business Intelligence to give you the edge</h1><br><img src="{{asset('images/bi.png')}}" alt=""></div>
         @else
             <iframe src="{{$iframe_etp->iframe}} " height="1000" width="100%" name="demo">
             </iframe>
@@ -12,7 +17,7 @@
     @endcan
     @can('isCFP')
         @if($iframe_cfp->iframe == null)
-            <img src="{{asset('images/construction.png')}}" alt="">
+        <div class="bi-content"><h1>Business Intelligence to give you the edge</h1><img src="{{asset('images/bi.png')}}" alt=""></div>
         @else
             <iframe src="{{$iframe_cfp->iframe}} " height="1000" width="100%" name="demo">
             </iframe>
