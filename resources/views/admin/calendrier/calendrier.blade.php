b1df0ae9ff9d117e17ab46944dd987c44c4b9b65
nic2000 <nicrah16@gmail.com>
2022-04-06 15:50:41 +0300

    commentaire agenda

--- resources/views/admin/calendrier/calendrier.blade.php
+++ resources/views/admin/calendrier/calendrier.blade.php
@@ -294,7 +294,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.3/jspdf.debug.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script> --}} --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script> --}}
 
     <script>
 
