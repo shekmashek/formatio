f87a75d6249d665e86e6db1830fad484fbbe2ef7
Nic <59892910+NicRuhmer@users.noreply.github.com>
2022-04-11 16:59:17 +0300

    Abonnement terminer

--- resources/views/superadmin/index_abonnement.blade.php
+++ resources/views/superadmin/index_abonnement.blade.php
@@ -11,9 +11,9 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-
+    <link rel="stylesheet" href="{{asset('assets/css/abonnement.css')}}">
 </head>
-@include('Views.superadmin.index-css')
+
 <body>
 
     {{-- <div class="abonnement_header">
--- resources/views/superadmin/listeAbonnement.blade.php
+++ resources/views/superadmin/listeAbonnement.blade.php
@@ -1,149 +1,229 @@
 @extends('layouts.admin')
 @section('title')
-    <p class="text_header m-0 mt-1">Abonnement</p>
+<p class="text_header m-0 mt-1">Abonnement</p>
 @endsection
 @section('content')
-<html>
-    <head>
-        <title> Pricing || page </title>
-        <link rel="stylesheet" href="{{ asset('bootstrapCss/css/bootstrap.min.css') }}">
-        <!-- CSS only -->
-{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> --}}
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
-<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link rel="stylesheet" href="{{asset('assets/css/abonnement.css')}}">
 
+<div class="container">
+    <div class="m-4">
+        <ul class="nav nav-tabs d-flex flex-row navigation_module" id="myTab">
+            <li class="nav-item">
+                <a href="#abonnement" class="nav-link active" data-bs-toggle="tab">Abonnements</a>
+            </li>
+            <li class="nav-item">
+                <a href="#facture" class="nav-link" data-bs-toggle="tab">Factures</a>
+            </li>
+        </ul>
 
-</head>
-@include('Views.superadmin.index-css')
-<body class="bg-light">
-    <div class="carHead">
-
-                {{-- <div class="row" style="color: white;">
-                    <div class="col-md-4 text-center"><img class="img-fluid rounded-3" style="width: 150px; height:80px;" src="{{ asset('logo/logo_white_background.jpg') }}" alt=""></div>
-                    <div class="col-md-4">
-                        <div class="bar">
-                              <p> Pages  </p>
-                               <p> Authentification </p>
-                                <p>Application  </p>
-                                <p>e-commerce  </p>
+        <div class="tab-content">
+            <div class="tab-pane fade show active" id="abonnement">
+                <div>
+                    <p class="h2 text-center mt-3 mb-5">Choisissez Votre Abonnement</p>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-4 col-md-6 ">
+                        <div class="card d-flex align-items-center justify-content-center">
+                            <div class="ribon"> <span class="bx bxs-paint"></span> </div>
+                            <p class="h-1 pt-5">DEMO</p> <span class="price"> <span class="number">0</span> <sup
+                                    class="sup">AR</sup></span>
+                            <ul class="mb-5 list-unstyled text-muted">
+                                <li><span class="bx bx-check me-2"></span>Test gratuit</li>
+                                <li><span class="bx bx-check me-2"></span>Creation de Compte Pro</li>
+                                <li><span class="bx bx-check me-2"></span>Accès aux Fonctionalité utiles</li>
+                            </ul>
+                            <div class="btn btn-primary">Commencer</div>
                         </div>
                     </div>
-                    <div class="col-md-4 text-center"><button class="buy_now"><i class="fa fa-plus"></i>&nbsp;<a href="{{route('abonnement.index')}}">Ajouter un nouvel abonnement</a>  </button></div>
-                </div> --}}
-
-
-
-            <h2 class="text-white font text-center mt-5"> Voici notre tarif </h2>
-            @isset($payant)
-                @foreach ($abn as  $abonnements)
-                    @foreach ($payant as $nom)
-                        @if($abonnements->id == $nom->type_abonnement_role->type_abonnement_id)
-                            <h2 class="text-white font text-center mt-5"> Votre offre est {{$abonnements->nom_type}} </h2>
-                        @endif
-                    @endforeach
-                @endforeach
-            @endisset
-            @isset($gratuit)
-                <h2 class="text-white font text-center mt-5"> Votre offre est {{$gratuit}} </h2>
-            @endisset
-
-            <div class="row align-items-center justify-content-center">
-                <div class="col-3 py-5">
-                    <div class="form-control bg-light g-0 d-flex px-1 py-1 btn_ligne btn-outline-none">
-                        <button class="btn mx-0 form-control btn_month"  onclick="month()" value="Mensuel" autofocus>Mensuel</button>&nbsp;
-                        <button class="btn mx-0 form-control btn_month"  onclick="year()" value="Annuel">Annuel</button>
+                    <div class="col-lg-4 col-md-6 ">
+                        <div class="card d-flex align-items-center justify-content-center">
+                            <div class="ribon"> <span class="bx bxs-star-half"></span> </div>
+                            <p class="h-1 pt-5">Standard</p> <span class="price"> <span class="number">200 000</span> <sup
+                                    class="sup">AR</sup>/ mois</span>
+                            <ul class="mb-5 list-unstyled text-muted">
+                                <li><span class="bx bx-check me-2"></span>Test gratuit</li>
+                                <li><span class="bx bx-check me-2"></span>Creation de Compte Pro</li>
+                                <li><span class="bx bx-check me-2"></span>Accès aux Fonctionalité utiles</li>
+                            </ul>
+                            <div class="btn btn-primary">Commencer</div>
+                        </div>
                     </div>
-<script>
-    $(document).on('load',function(load)){
-        document.getElementById("mensuel").style.color = 'red';
-});
-
-</script>
+                    <div class="col-lg-4 col-md-6 ">
+                        <div class="card d-flex align-items-center justify-content-center">
+                            <div class="ribon"> <span class="bx bxs-diamond"></span> </div>
+                            <p class="h-1 pt-5">Standard</p> <span class="price"> <span class="number">2 400 000</span> <sup
+                                    class="sup">AR</sup>/ an</span>
+                            <ul class="mb-5 list-unstyled text-muted">
+                                <li><span class="bx bx-check me-2"></span>Test gratuit</li>
+                                <li><span class="bx bx-check me-2"></span>Creation de Compte Pro</li>
+                                <li><span class="bx bx-check me-2"></span>Accès aux Fonctionalité utiles</li>
+                            </ul>
+                            <div class="btn btn_primary">Commencer</div>
+                        </div>
+                    </div>
+                </div><br><br>
+                <div class="row mt-3">
+                    <div class="col-lg-4 col-md-6 ">
+                        <div class="card d-flex align-items-center justify-content-center">
+                            <div class="ribon_premium"> <span class="bx bxs-paint"></span> </div>
+                            <p class="h-1 pt-5">DEMO Premium</p> <span class="price"> <span class="number">0</span> <sup
+                                    class="sup">AR</sup></span>
+                            <ul class="mb-5 list-unstyled text-muted">
+                                <li><span class="bx bx-check me-2"></span>Test gratuit</li>
+                                <li><span class="bx bx-check me-2"></span>Creation de Compte Pro</li>
+                                <li><span class="bx bx-check me-2"></span>Accès aux Fonctionalité utiles</li>
+                            </ul>
+                            <div class="btn btn-primary btn_premium">Commencer</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 ">
+                        <div class="card d-flex align-items-center justify-content-center">
+                            <div class="ribon_premium"> <span class="bx bxs-star-half"></span> </div>
+                            <p class="h-1 pt-5">Premium</p> <span class="price"> <span class="number">300 000</span> <sup class="sup">AR</sup>/ mois</span>
+                            <ul class="mb-5 list-unstyled text-muted">
+                                <li><span class="bx bx-check me-2"></span>Test gratuit</li>
+                                <li><span class="bx bx-check me-2"></span>Creation de Compte Pro</li>
+                                <li><span class="bx bx-check me-2"></span>Accès aux Fonctionalité utiles</li>
+                            </ul>
+                            <div class="btn btn-primary btn_premium">Commencer</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 ">
+                        <div class="card d-flex align-items-center justify-content-center">
+                            <div class="ribon_premium"> <span class="bx bxs-diamond"></span> </div>
+                            <p class="h-1 pt-5">Premium</p> <span class="price"> <span class="number">2 600 000</span> <sup
+                                    class="sup">AR</sup>/ an</span>
+                            <ul class="mb-5 list-unstyled text-muted">
+                                <li><span class="bx bx-check me-2"></span>Test gratuit</li>
+                                <li><span class="bx bx-check me-2"></span>Creation de Compte Pro</li>
+                                <li><span class="bx bx-check me-2"></span>Accès aux Fonctionalité utiles</li>
+                            </ul>
+                            <div class="btn btn_primary btn_premium">Commencer</div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="tab-pane fade" id="facture">
 
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-10">
-            <center>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <div class="carHead">
+    <h2 class="text-white font text-center mt-5"> Voici notre tarif </h2>
+    @isset($payant)
+    @foreach ($abn as $abonnements)
+    @foreach ($payant as $nom)
+    @if($abonnements->id == $nom->type_abonnement_role->type_abonnement_id)
+    <h2 class="text-white font text-center mt-5"> Votre offre est {{$abonnements->nom_type}} </h2>
+    @endif
+    @endforeach
+    @endforeach
+    @endisset
+    @isset($gratuit)
+    <h2 class="text-white font text-center mt-5"> Votre offre est {{$gratuit}} </h2>
+    @endisset
+
+    <div class="row align-items-center justify-content-center">
+        <div class="col-3 py-5">
+            <div class="form-control bg-light g-0 d-flex px-1 py-1 btn_ligne btn-outline-none">
+                <button class="btn mx-0 form-control btn_month" onclick="month()" value="Mensuel"
+                    autofocus>Mensuel</button>&nbsp;
+                <button class="btn mx-0 form-control btn_month" onclick="year()" value="Annuel">Annuel</button>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
             <div class="row d-flex flex-row flex-wrap justify-content-evenly">
                 @isset($gratuit)
-                    @foreach($offregratuit as $offre)
-                        <div class="card_repeat bg-white">
-                            <div class="py-3">
-                                <button class="btn_premium">
-                                    <h4>Gratuit</h4>
-                                </button>
-                                <br>
-                                    <p><h1 class="gratuit">0 Ar</h1></p><br>
+                @foreach($offregratuit as $offre)
+                <div class="card_repeat bg-white">
+                    <div class="py-3">
+                        <button class="btn_premium">
+                            <h4>Gratuit</h4>
+                        </button>
+                        <br>
+                        <p>
+                        <h1 class="gratuit">0 Ar</h1>
+                        </p><br>
 
 
-                                    <p class="th_color"> <i class="fal fa-check" style="font-size: 10px; padding: 4px; font-weight:bold;"></i>&nbsp;&nbsp; {{$offre->limite}} collaborations avec les {{$offre->type_abonne->abonne_name}}</p>
+                        <p class="th_color"> <i class="fal fa-check"
+                                style="font-size: 10px; padding: 4px; font-weight:bold;"></i>&nbsp;&nbsp;
+                            {{$offre->limite}} collaborations avec les {{$offre->type_abonne->abonne_name}}</p>
 
-                                <p></p>
-                            </div>
-                        </div>
-                    @endforeach
+                        <p></p>
+                    </div>
+                </div>
+                @endforeach
                 @endisset
-               @foreach ($typeAbonnement as $types)
-                    @foreach ($tarif as $tf)
-                        @if($tf->type_abonnement_role_id == $types->types_id)
+                @foreach ($typeAbonnement as $types)
+                @foreach ($tarif as $tf)
+                @if($tf->type_abonnement_role_id == $types->types_id)
 
-                            <div class="card_repeat bg-white">
-                                <div class="py-3">
+                <div class="card_repeat bg-white">
+                    <div class="py-3">
 
-                                    <button class="btn_premium">
-                                        <h4>{{ $types->nom_type }}</h4>
-                                    </button>
-                                    <br>
-                                    <h1><input disabled value=" <?php echo number_format($tf->tarif, 2, ',', '.') ;  ?> Ar" style="text-align:center; border:none; width:300px; background-color:white;" id="prixMensuel"></h1><p></p>
-                                    @foreach ($tarifAnnuel as $tfAnn)
-                                        @if($tfAnn->type_abonnement_role_id == $types->types_id)
-                                            <h1><input disabled value=" <?php echo number_format($tfAnn->tarif, 2, ',', '.') ;  ?> Ar" style="display:none;text-align:center; border:none; width:300px; background-color:white;" id="prixAnnuel"></h1><p></p>
-                                        @endif
-                                    @endforeach
-                                        {{-- <p><h1>@php
-                                            echo number_format($tf->tarif, 2, ',', '.');
-                                        @endphp   Ar</h1></p><br> --}}
-
-                                    <ul>
-                                        <p> <i class="fal fa-check" style="font-size: 10px; padding: 4px; font-weight:bold;"></i>&nbsp;&nbsp; Collaboration illimité </p>
-                                    </ul>
-                                    <p></p>
-                                    <button class="form-control btn_join" id="abonnerMensuel"> <a href="{{route('abonnement-page',['id'=>$tf->id])}}"> S'abonner  </a> &nbsp;&nbsp;  <i class="fal fa-arrow-right"></i> </button><br>
-
-                                    @foreach ($tarifAnnuel as $tfAnn)
-                                        @if($tfAnn->type_abonnement_role_id == $types->types_id)
-                                            <button class="form-control btn_join" id="abonnerAnnuel" style="display: none"> <a href="{{route('abonnement-page',['id'=>$tfAnn->id])}}"> S'abonner  </a> &nbsp;&nbsp;  <i class="fal fa-arrow-right"></i> </button><br>
-                                        @endif
-                                    @endforeach
-
-
-                                </div>
-                            </div>
+                        <button class="btn_premium">
+                            <h4>{{ $types->nom_type }}</h4>
+                        </button>
+                        <br>
+                        <h1><input disabled value=" <?php echo number_format($tf->tarif, 2, ',', '.') ;  ?> Ar"
+                                style="text-align:center; border:none; width:300px; background-color:white;"
+                                id="prixMensuel"></h1>
+                        <p></p>
+                        @foreach ($tarifAnnuel as $tfAnn)
+                        @if($tfAnn->type_abonnement_role_id == $types->types_id)
+                        <h1><input disabled value=" <?php echo number_format($tfAnn->tarif, 2, ',', '.') ;  ?> Ar"
+                                style="display:none;text-align:center; border:none; width:300px; background-color:white;"
+                                id="prixAnnuel"></h1>
+                        <p></p>
                         @endif
-                    @endforeach
-            @endforeach
+                        @endforeach
+
+
+                        <ul>
+                            <p> <i class="fal fa-check"
+                                    style="font-size: 10px; padding: 4px; font-weight:bold;"></i>&nbsp;&nbsp;
+                                Collaboration illimité </p>
+                        </ul>
+                        <p></p>
+                        <button class="form-control btn_join" id="abonnerMensuel"> <a
+                                href="{{route('abonnement-page',['id'=>$tf->id])}}"> S'abonner </a> &nbsp;&nbsp; <i
+                                class="fal fa-arrow-right"></i> </button><br>
+
+                        @foreach ($tarifAnnuel as $tfAnn)
+                        @if($tfAnn->type_abonnement_role_id == $types->types_id)
+                        <button class="form-control btn_join" id="abonnerAnnuel" style="display: none"> <a
+                                href="{{route('abonnement-page',['id'=>$tfAnn->id])}}"> S'abonner </a> &nbsp;&nbsp; <i
+                                class="fal fa-arrow-right"></i> </button><br>
+                        @endif
+                        @endforeach
+
+
+                    </div>
+                </div>
+                @endif
+                @endforeach
+                @endforeach
 
 
             </div>
 
-        </center>
-    </div>
+        </div>
 
-    <div class="col-md-1"></div>
-</div>
-<hr>
-<br><br>
+        <div class="col-md-1"></div>
     </div>
+</div> --}}
 
-</body>
-<script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <script>
-     function month()
+    function month()
         {
            document.getElementById('prixMensuel').style.display = "block";
             document.getElementById('abonnerMensuel').style.display = "block";
@@ -158,5 +238,10 @@
             document.getElementById('prixAnnuel').style.display = "block";
         }
 </script>
-</html>
+<script>
+    $(document).on('load',function(load)){
+        document.getElementById("mensuel").style.color = 'red';
+});
+
+</script>
 @endsection
