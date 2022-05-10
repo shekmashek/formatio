9de101ed2f20ce16794bb6608aa7d35a11eba145
nic2000 <nicrah16@gmail.com>
2022-05-10 17:04:23 +0300

    reglage affichage abonnement actuel

--- app/Http/Controllers/AbonnementController.php
+++ app/Http/Controllers/AbonnementController.php
@@ -227,6 +227,7 @@ class AbonnementController extends Controller
         if (Gate::allows('isReferent')) {
 
             $responsable =$this->fonct->findWhere('responsables',['user_id'],[Auth::user()->id]);
+            $entreprise_id = $responsable[0]->entreprise_id;
             $test_abonne =$this->fonct->findWhere('abonnements',['entreprise_id','status'],[$responsable[0]->entreprise_id,'En attente']);
 
 
@@ -322,8 +323,9 @@ class AbonnementController extends Controller
 
 
             if ($test_abonne) {
-                $payant = abonnement::with('type_abonnement_role')->where('entreprise_id', $responsable[0]->entreprise_id)->get();
-                return view('superadmin.listeAbonnement', compact('facture_suivant','abonnement_actuel','annee','mois','net_ttc','tva','facture','abn', 'payant', 'typeAbonne_id', 'tarifAnnuel', 'offregratuit', 'typeAbonnement', 'tarif'));
+                $payant = $this->fonct->findWhere("v_type_abonnement_etp",['entreprise_id'],[$entreprise_id]);
+                // $payant = abonnement::with('type_abonnement_role')->where('entreprise_id', $responsable[0]->entreprise_id)->get();
+                return view('superadmin.listeAbonnement', compact('facture_suivant','abonnement_actuel','annee','mois','net_ttc','tva','facture', 'payant', 'typeAbonnement'));
             }
             if ($test_abonne == false) {
                 $gratuit = "Gratuite";
