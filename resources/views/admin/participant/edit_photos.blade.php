ac8b0682cde6c8ae8ebc7156bcec36a0a9933fb4
DevsG1 <raharimananagael@gmail.com>
2022-05-25 16:59:51 +0300

    modif boutton

--- resources/views/admin/participant/edit_photos.blade.php
+++ resources/views/admin/participant/edit_photos.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <div class="col" style="margin-left: 25px">
-    <a href="{{route('profile_stagiaire')}}" role="button" class="btn btn_precedent me-1 my-2">  Retour</a>
+    <a href="{{route('profile_stagiaire')}}" role="button" class="btn btn_precedent me-1 my-2"><i class="bx bxs-chevron-left me-1"></i>   Retour</a>
   </div>
 <center>
  {{-- si l'utiliisateur a cliqué sur enregistrer sans choisir un fichier--}}
