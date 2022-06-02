5581e5ce32649bb1201847621a746876b83029a3
nic2000 <nicrah16@gmail.com>
2022-06-02 09:46:00 +0300

    ajout requete pour mr

--- bdd_et_view_version_1/requete_mr/emargement.sql
+++ bdd_et_view_version_1/requete_mr/emargement.sql
@@ -1,15 +1,15 @@
 SELECT dtls.date_detail,dtls.h_debut as debut_seance,
        dtls.h_fin as fin_seance,
        p.h_entree as entree_stagiaire, p.h_sortie as sortie_stagiaire,
-       frm.nom_formateur,frm.prenom_formateur,
+       frm.nom_formateur,frm.prenom_formateur,frm.url_photo as url_photo_formateur,
        gp.nom_groupe as nom_session,
        note,
-       stg.nom_stagiaire,stg.prenom_stagiaire,stg.entreprise_id,
-       etp.nom_etp as entreprise,
+       stg.nom_stagiaire,stg.prenom_stagiaire,stg.url_photo as url_photo_stagiaire,stg.entreprise_id,
+       etp.nom_etp as entreprise,etp.url_logo as url_logo_etp,
        dtls.cfp_id,
-       cfp.nom as organisme_formation,
+       cfp.nom as organisme_formation,cfp.url_logo as url_logo_cfp,
 CASE p.status
-        WHEN '1' THEN 'présent'
+        WHEN '1' THEN 'present'
         WHEN '0' THEN 'absent'
 END as status_presence
 FROM presences as p
--- bdd_et_view_version_1/requete_mr/qualite.sql
+++ bdd_et_view_version_1/requete_mr/qualite.sql
@@ -1,9 +1,9 @@
 select desc_champ.descr_champs,qst_mere.qst_mere,qst_mere.desc_reponse,qst_fille.qst_fille, reponse.reponse_desc_champ,reponse.points,
-stg.nom_stagiaire,stg.prenom_stagiaire,stg.entreprise_id,
+stg.nom_stagiaire,stg.prenom_stagiaire,stg.url_photo as url_photo_stagiaire,stg.entreprise_id,
 gp.nom_groupe,
-etp.nom_etp as entreprise,
+etp.nom_etp as entreprise,etp.url_logo as url_logo_etp,
 reponse.cfp_id,
-cfp.nom as organisme_formation
+cfp.nom as organisme_formation,cfp.url_logo as url_logo_cfp
 FROM reponse_evaluationchaud as reponse
 LEFT JOIN stagiaires AS stg ON
 stg.id = reponse.stagiaire_id
