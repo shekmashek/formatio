e3eff98c04e12fe64f13873ff347a8765c244b99
FranciscoNoam <91475507+FranciscoNoam@users.noreply.github.com>
2022-06-10 13:55:35 +0300

    ok

--- resources/views/cfp/profile_entreprise.blade.php
+++ resources/views/cfp/profile_entreprise.blade.php
@@ -515,7 +515,7 @@
 
           {{-- Tab 1 content --}}
         </div>
-        <div class="tab-pane fade show active" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
+        {{-- <div class="tab-pane fade show active" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
             <div class="row mt-2">
                 <div class="col-12 col-lg-6">
                     @if(Session::has('success'))
@@ -642,7 +642,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <div class="tab-pane fade" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
           Tab 3 content
         </div>
